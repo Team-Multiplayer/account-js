43578a0b0ba79c8aeb59b748977f650d66bb4291
Danilo Elias <daniloselias@gmail.com>
2021-01-31 19:12:25 -0300

    Corrigir estilos

--- src/views/components/Footer.js
+++ src/views/components/Footer.js
@@ -1,7 +1,7 @@
 let Footer = {
   render: async () => {
     let view = `
-    <footer class="footer">
+    <footer class="footer mt-5">
       <p class="footer--title text-white">AccountBank CopyRight 2021.</p>
       <div class="footer--logo">
         <span class="purple--text">Acc</span>ount
