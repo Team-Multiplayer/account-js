d1d413a7474ed575118affd114b5a5c0329117b8
Danilo Elias <daniloselias@gmail.com>
2021-01-31 15:51:15 -0300

    Adicionar biblioteca de funções úteis

---
+++ src/service/Utils.js
@@ -0,0 +1,20 @@
+let Utils = {
+  // parser URL
+  parseRequestURL: () => {
+    let url = location.hash.slice(1).toLocaleLowerCase() || '/';
+    let r = url.split('/')
+    let request = {
+      resource: null,
+      id: null,
+      verb: null
+    }
+
+    request.resource = r[1]
+    request.id = r[2]
+    request.verb = r[3]
+
+    return request;
+  }
+}
+
+export default Utils;
