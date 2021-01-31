90aff0ef69df2b55cb2e7fc21ff0e14a04f72230
Danilo Elias <daniloselias@gmail.com>
2021-01-31 17:28:48 -0300

    Alterar forma de exportar e adicionar funções de validação do formulário

--- src/service/Utils.js
+++ src/service/Utils.js
@@ -1,20 +1,31 @@
-let Utils = {
-  // parser URL
-  parseRequestURL: () => {
-    let url = location.hash.slice(1).toLocaleLowerCase() || '/';
-    let r = url.split('/')
-    let request = {
-      resource: null,
-      id: null,
-      verb: null
-    }
-
-    request.resource = r[1]
-    request.id = r[2]
-    request.verb = r[3]
-
-    return request;
+const parseRequestURL = () => {
+  let url = location.hash.slice(1).toLocaleLowerCase() || '/';
+  let r = url.split('/')
+  let request = {
+    resource: null,
+    id: null,
+    verb: null
   }
+
+  request.resource = r[1]
+  request.id = r[2]
+  request.verb = r[3]
+
+  return request;
 }
 
-export default Utils;
+const inputValid =  (obj) => {
+  obj.classList.remove('is-invalid');
+  obj.classList.add('is-valid');
+}
+
+const inputInvalid = (obj) => {
+  obj.classList.remove('is-valid');
+  obj.classList.add('is-invalid');
+}
+
+  export {
+    parseRequestURL,
+    inputValid,
+    inputInvalid,
+  };
