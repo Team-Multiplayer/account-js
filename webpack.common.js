8e94042a0b488722595de3a13b7e486dc30c14a9
Danilo Elias <daniloselias@gmail.com>
2021-01-31 15:52:02 -0300

    Adidionar as imagens usadas

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,13 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const pathToMainJs = require.resolve("./src/app.js");
 const pathToIndexCss = require.resolve("./src/css/style.css");
 const pathToIndexHtml = require.resolve("./src/index.html");
+const pathToBanner = require.resolve("./src/img/banner.jpg");
+const pathToBannerCrypto = require.resolve("./src/img/banner-crypto.jpg");
+const pathToBannerMobile = require.resolve("./src/img/banner-mobile.jpg");
+const pathToHome1 = require.resolve("./src/img/home-1.jpg");
+const pathToHome2 = require.resolve("./src/img/home-2.png");
+const pathToHome3 = require.resolve("./src/img/home-3.png");
+const pathToLogin = require.resolve("./src/img/login.jpg");
 
 module.exports =  {
   entry: [
@@ -12,6 +19,13 @@ module.exports =  {
     pathToMainJs,
     pathToIndexHtml,
     pathToIndexCss,
+    pathToBanner,
+    pathToBannerCrypto,
+    pathToBannerMobile,
+    pathToHome1,
+    pathToHome2,
+    pathToHome3,
+    pathToLogin
   ],
   plugins: [
     new CleanWebpackPlugin(),
